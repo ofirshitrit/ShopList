385bf7026b48ab16bcc1db8292001d30c5b68d5a
OfirShitrit <ofirha18@gmail.com>
2024-05-22 18:03:58 +0300

    handle the square later

--- components/Item.js
+++ components/Item.js
@@ -5,7 +5,7 @@ import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 const Item = (props) => {
   return (
     <View style={styles.item}>
-        <View style = {styles.square}></View>
+        {/* <View style = {styles.square}></View> */}
         <Text style = {styles.itemText}>{props.text}</Text>
         {/* <Image ></Image> */}
     </View>
@@ -23,19 +23,19 @@ const styles = StyleSheet.create({
     // height:100,
     // alignItems: "center",
     // justifyContent: "center",
-    flexDirection: "row",
-    alignItems: 'center',
-    flexWrap: 'wrap'
+    // flexDirection: "row",
+    // alignItems: 'center',
+    // flexWrap: 'wrap'
   },
-  square:{
-    width: 24,
-    height: 24,
-    backgroundColor: '#55BCF6',
-    opacity: 0.4,
-    borderRadius: 4,
-    marginRight: 15,
+  // square:{
+  //   width: 24,
+  //   height: 24,
+  //   backgroundColor: '#55BCF6',
+  //   opacity: 0.4,
+  //   borderRadius: 4,
+  //   marginRight: 15,
 
-  },
+  // },
   
   itemText:{
     fontSize: 25,
